528e2a616501804df0126bcf4aaa03847c4fcd23
Виталий Иванов <shahmatis@example.com>
2024-01-22 19:27:31 +0300

    first commit

---
+++ HW1/data.js
@@ -0,0 +1,66 @@
+export const workouts = `
+[
+    {
+        "id": "1",
+        "name_of_workout": "HATHA YOGA",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "5"
+    },
+    {
+        "id": "2",
+        "name_of_workout": "Aqua Cardio",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "3"
+    },
+    {
+        "id": "3",
+        "name_of_workout": "PILATES",
+        "time_of_workout": "11:00 - 11:50",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "18"
+    },
+    {
+        "id": "4",
+        "name_of_workout": "BODY UP",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "20"
+    },
+    {
+        "id": "5",
+        "name_of_workout": "Aqua Dumbells",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "8"
+    },
+    {
+        "id": "6",
+        "name_of_workout": "Legs & Buts",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "19"
+    },
+    {
+        "id": "7",
+        "name_of_workout": "FUNCTIONAL",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "13"
+    },
+    {
+        "id": "8",
+        "name_of_workout": "ONLY WOMEN",
+        "time_of_workout": "09:00 - 09:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "7"
+    },
+    {
+        "id": "9",
+        "name_of_workout": "FITNESS MIX",
+        "time_of_workout": "11:30 - 12:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "14"
+    }
+]`
